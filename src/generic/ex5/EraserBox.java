34e3e4ae9ad98b5764a3cd283a6633d336c9a0ec
jisulee-shsf <jisulee.shsf@gmail.com>
2026-08-29 13:06:16 +0900

    feat: [제네릭] 타입 이레이저

---
+++ src/generic/ex5/EraserBox.java
@@ -0,0 +1,12 @@
+package generic.ex5;
+
+public class EraserBox<T> {
+
+//    public boolean instanceCheck(Object param) {
+//        return param instanceof T;
+//    }
+
+//    public T create() {
+//        return new T();
+//    }
+}
