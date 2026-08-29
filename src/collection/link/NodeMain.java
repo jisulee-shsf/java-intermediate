1dc9229f62a7d43e3d40f9258a343508f2b65325
jisulee-shsf <jisulee.shsf@gmail.com>
2026-08-30 02:56:08 +0900

    feat: [컬렉션 프레임워크 | LinkedList] 노드와 연결1

---
+++ src/collection/link/NodeMain.java
@@ -0,0 +1,18 @@
+package collection.link;
+
+public class NodeMain {
+    public static void main(String[] args) {
+        // 노드 생성 및 연결
+        Node first = new Node("A");
+        first.next = new Node("B");
+        first.next.next = new Node("C");
+
+        // 모든 노드 탐색
+        Node x = first;
+        System.out.println("x = " + x);
+        while (x != null) {
+            System.out.println("x.item = " + x.item);
+            x = x.next;
+        }
+    }
+}
