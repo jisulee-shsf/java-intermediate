d2059c0ea50dae643cfb2195f933b1a5197fece8
jisulee-shsf <jisulee.shsf@gmail.com>
2026-09-06 09:51:28 +0900

    test: [컬렉션 프레임워크] Stack

---
+++ src/collection/deque/StackMain.java
@@ -0,0 +1,21 @@
+package collection.deque;
+
+import java.util.Stack;
+
+public class StackMain {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println(stack); // [1, 2, 3]
+
+        System.out.println(stack.peek()); // 3
+
+        System.out.println(stack.pop()); // 3
+        System.out.println(stack.pop()); // 2
+        System.out.println(stack.pop()); // 1
+        System.out.println(stack); // []
+    }
+}
