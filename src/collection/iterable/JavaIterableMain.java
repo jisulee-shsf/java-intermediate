e50cd39d360f1b23e2a7e0c155e46c559e256b98
jisulee-shsf <jisulee.shsf@gmail.com>
2026-09-06 14:17:04 +0900

    test: [컬렉션 프레임워크 | 순회] 자바의 Iterable, Iterator

---
+++ src/collection/iterable/JavaIterableMain.java
@@ -0,0 +1,58 @@
+package collection.iterable;
+
+import java.util.*;
+
+public class JavaIterableMain {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        printAll(list.iterator());
+        forEach(list);
+        /*
+        class java.util.ArrayList$Itr
+        1
+        2
+        3
+        class java.util.ArrayList
+        1
+        2
+        3
+        */
+
+        Set<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+
+        printAll(set.iterator());
+        forEach(set);
+        /*
+        class java.util.HashMap$KeyIterator
+        1
+        2
+        3
+        class java.util.HashSet
+        1
+        2
+        3
+        */
+    }
+
+    private static void printAll(Iterator<Integer> iterator) {
+        System.out.println(iterator.getClass());
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
+    private static void forEach(Iterable<Integer> iterable) {
+        System.out.println(iterable.getClass());
+        for (Integer i : iterable) {
+            System.out.println(i);
+        }
+    }
+}
